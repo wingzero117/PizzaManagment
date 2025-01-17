e1d48d87149ab0cb1234c291fd9b8fe4f232c92d
cosif1 <cosif@asu.edu>
2025-01-17 10:04:32 -0700

    permission changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
+app.use(cors());
 app.use(express.json());
 
 AppDataSource.initialize().then(()=> {
